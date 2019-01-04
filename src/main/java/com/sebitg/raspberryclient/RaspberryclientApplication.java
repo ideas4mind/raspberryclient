2623c32f9688421be1865d63b2e888470046dd93
Sebastian Mekal <sebastian.mekal@iquestgroup.com>
2019-01-04 15:29:33 +0100

    Initial commit

---
+++ src/main/java/com/sebitg/raspberryclient/RaspberryclientApplication.java
@@ -0,0 +1,14 @@
+package com.sebitg.raspberryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryclientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RaspberryclientApplication.class, args);
+  }
+
+}
+
